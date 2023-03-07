47f653dbf20a2f6f2c82dcf158872f5d79afd216
mukhnaya <mosesmukhanya@gmail.com>
2023-03-07 17:49:23 +0000

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ las_dig = "Last digit of"
 great_5 = "and is greater than 5"
 les_6 = "and is less than 6 and not 0"
 is_zero = "and is 0"
-if (last_dig > 5):
+if (number > 5):
     print("{} {} is {} {}".format(las_dig, number, last_dig, great_5))
-elif (last_dig < 6 and last_dig != 0):
+elif (number < 6 and last_dig != 0):
     print("{} {} is {} {}".format(las_dig, number, last_dig, les_6))
 else:
     print("{} {1} is {} {}".format(las_dig, number, last_dig, is_zero))
