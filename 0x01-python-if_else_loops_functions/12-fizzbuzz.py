75f1bba0f19227be865314cab31c6de0dc8f9dbf
mukhnaya <mosesmukhanya@gmail.com>
2023-03-08 07:44:57 +0000

    task 11

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    fizz = "Fizz"
+    buzz = "Buzz"
+    fizzbuzz = "FizzBuzz"
+    last = 100
+    for pau in range(1, 101):
+        if pau % 3 == 0 and pau % 5 == 0:
+            print("{}".format(fizzbuzz), end=" ")
+        if pau % 3 == 0:
+            print("{}".format(fizz), end=" ")
+        if pau % 5 == 0:
+            print("{}".format(buzz), end=" ")
+        if pau == 100:
+            print("{}".format(last))
