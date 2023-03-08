9659ed5f4ccf2e93f298462ec27acbde1df7d03c
mukhnaya <mosesmukhanya@gmail.com>
2023-03-08 08:21:17 +0000

    task 12

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
+    str1 = ""
     for pau in range(len(str)):
-        str[n] = " "
+        if pau == n:
+            str1[pau] = " "
+        else:
+            str1[pau] = str[pau]
+    print(str1)
