985fa5e1cbc8f1967d6e9a6a0a104789146bbb88
mukhnaya <mosesmukhanya@gmail.com>
2023-03-08 06:52:09 +0000

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    final_string =""
+    final_string = ""
     for pau in str:
         if pau in 'abcdefghijklmnopqrstuvwqxyz':
             mos = ord(pau)
