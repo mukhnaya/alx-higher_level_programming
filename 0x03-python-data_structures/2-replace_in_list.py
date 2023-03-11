437923606ef0c10594a90c7819d046f387e36f7b
mukhnaya <mosesmukhanya@gmail.com>
2023-03-11 15:58:49 +0000

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-
+def replace_in_list(my_list, idx, element):
+    pau = len(my_list) - 1
+    if idx < 0:
+        return my_list
+    elif idx > pau:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
