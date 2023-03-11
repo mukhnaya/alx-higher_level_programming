5a4dd944fe75222fdc395fc7347bd7ce83bfb260
mukhnaya <mosesmukhanya@gmail.com>
2023-03-11 14:51:42 +0000

    updated

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    '''
-    get length of the list and then print
-    use a variable to determin length of list
-    '''
     for pau in my_list:
         print("{}".format(pau))
