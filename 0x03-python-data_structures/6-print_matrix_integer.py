9338403b2124f15f799d97ce63092aa458b62331
mukhnaya <mosesmukhanya@gmail.com>
2023-03-12 10:59:20 +0000

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -8,13 +8,3 @@ def print_matrix_integer(matrix=[[]]):
             else:
                 print("{:d}".format(matrix[jul][mos]), end="")
         print()
-
-matrix = [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9]
-        ]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
