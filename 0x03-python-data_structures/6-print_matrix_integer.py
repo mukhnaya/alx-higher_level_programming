89594a6b7272a0848213c1fe3c272ef408d0a5be
mukhnaya <mosesmukhanya@gmail.com>
2023-03-12 11:01:14 +0000

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     pau = len(matrix[0])
     for jul in range(pau):
         for mos in range(pau):
-            if mos < pau -1:
+            if mos < pau - 1:
                 print("{:d}".format(matrix[jul][mos]), end=" ")
             else:
                 print("{:d}".format(matrix[jul][mos]), end="")
