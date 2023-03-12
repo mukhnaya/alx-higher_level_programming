11de6bad989621f12da210bebe76325db29de8c9
mukhnaya <mosesmukhanya@gmail.com>
2023-03-12 16:09:55 +0000

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    pau = len(my_list) - 1
+    x = pau
+    if pau == 0:
+        return None
+    else:
+        while pau > 1:
+            mos = 0
+            while mos < pau:
+                if my_list[mos] > my_list[mos + 1]:
+                    temp = my_list[mos]
+                    my_list[mos] = my_list[mos + 1]
+                    my_list[mos + 1] = temp
+                mos +=1
+            pau -=1       
+    return my_list[x]
