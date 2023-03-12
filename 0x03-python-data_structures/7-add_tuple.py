b2b8022b840dddc4ac0728b289ef62b488fef1ca
mukhnaya <mosesmukhanya@gmail.com>
2023-03-12 14:23:02 +0000

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-        pau = len(tuple_a)
-        mos = len(tuple_b)
-        if pau == 2 and mos == 2:
-            c = tuple_a[0] + tuple_b[0]
-            d = tuple_a[1] + tuple_b[1]
-        elif pau == 2 and mos == 1:
-            c = tuple_a[0] + tuple_b[0]
-            d = tuple_a[1] + 0
-        elif pau == 2 and mos == 0:
-            c = tuple_a[0] + 0
-            d = tuple_a[1] + 0
-        elif pau == 1 and mos == 2:
-            c = tuple_a[0] + tuple_b[0]
-            d = 0 + tuple_b[1]
-        elif pau == 0 and mos == 2:
-            c = 0 + tuple_b[0]
-            d = 0 + tuple_b[1]
-        elif pau == 1 and mos == 1:
-            c = tuple_a[0] + tuple_b[0]
-            d = 0 + 0
-        elif pau == 0 and mos == 1:
-            c = 0 + tuple_b[0]
-            d = 0 + 0
-        elif pau == 0 and mos == 0:
-            c = 0 + 0
-            d = 0 + 0
-        elif pau == 1 and mos == 0:
-            c = tuple_a[0] + 0
-            d = 0 + 0
-        x = c, d
-        return x
+    pau = len(tuple_a)
+    mos = len(tuple_b)
+    if pau > 1 and mos > 1:
+        c = tuple_a[0] + tuple_b[0]
+        d = tuple_a[1] + tuple_b[1]
+    elif pau > 1 and mos == 1:
+        c = tuple_a[0] + tuple_b[0]
+        d = tuple_a[1] + 0
+    elif pau > 1 and mos == 0:
+        c = tuple_a[0] + 0
+        d = tuple_a[1] + 0
+    elif pau == 1 and mos > 2:
+        c = tuple_a[0] + tuple_b[0]
+        d = 0 + tuple_b[1]
+    elif pau == 0 and mos > 2:
+        c = 0 + tuple_b[0]
+        d = 0 + tuple_b[1]
+    elif pau == 1 and mos == 1:
+        c = tuple_a[0] + tuple_b[0]
+        d = 0 + 0
+    elif pau == 0 and mos == 1:
+        c = 0 + tuple_b[0]
+        d = 0 + 0
+    elif pau == 0 and mos == 0:
+        c = 0 + 0
+        d = 0 + 0
+    elif pau == 1 and mos == 0:
+        c = tuple_a[0] + 0
+        d = 0 + 0
+    x = c, d
+    return x
