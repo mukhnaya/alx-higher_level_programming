d5afef373ceca40efb055ea370e1922360281a10
mukhnaya <mosesmukhanya@gmail.com>
2023-03-27 23:34:50 +0000

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,6 @@ def safe_print_list(my_list=[], x=0):
                 jos = 0
                 for jul in range(jos, x):
                     print("{:d}".format(my_list[jul]), end="")
-                print()
         except IndexError:
             pass
         print()
