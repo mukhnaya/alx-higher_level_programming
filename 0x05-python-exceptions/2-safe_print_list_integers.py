910db65eb1756a524180bdcd1a2ef175c8c3178d
mukhnaya <mosesmukhanya@gmail.com>
2023-03-27 23:52:14 +0000

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    int pau = 0
+    pau = 0
     for mos in range(x):
         try:
-            print("{:d}".format(my_list[mos]))
+            print("{:d}".format(my_list[mos]), end="")
             pau = pau + 1
         except (TypeError, ValueError):
             pass
