3e4636945c2907039eae9b261edbbeb8273b9bc3
mukhnaya <mosesmukhanya@gmail.com>
2023-04-04 07:49:19 +0000

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,22 @@
+#!/usr/python3
+'''class square to for getters and setters'''
+
+
+class Square:
+    '''body of the Square class'''
+
+    def __init(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        self.size = value
+        if type (size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        return self.__size * self.__size
