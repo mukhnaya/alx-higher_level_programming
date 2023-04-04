1f0777bc8ae5f81b016d3a22b2a5a60eb4f03ee6
mukhnaya <mosesmukhanya@gmail.com>
2023-04-04 06:40:42 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''creates a square class'''
+class Square:
+    '''the class is not empty'''
+
+    def __init__(self, size):
+        '''initialize size'''
+
+        self.__size = size
