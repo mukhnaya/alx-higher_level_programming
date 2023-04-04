50308a3a5a67e21fb2eb64a3ba22adc58adcb613
mukhnaya <mosesmukhanya@gmail.com>
2023-04-04 08:06:53 +0000

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,19 +5,23 @@
 class Square:
     '''body of the Square class'''
 
-    def __init(self, size=0):
-        self.__size = size
+    def __init__(self, size=0):
+        self.size = size
 
     @property
     def size(self):
+        '''returns size'''
         return self.__size
 
+    @size.setter
     def size(self, value):
-        self.size = value
-        if type(size) is not int:
+        '''sets the value of size'''
+        self.__size = value
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        '''gets area'''
         return self.__size * self.__size
