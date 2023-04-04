f831d0528ef802104c8b2353a05804da835d33dc
mukhnaya <mosesmukhanya@gmail.com>
2023-04-04 06:50:03 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@
 class Square:
     '''this body is not empty'''
 
-    __size
+    __size = 0
     if (type(size) is not int:
             raise TypeError("size must be an integer")
     if size < 0:
