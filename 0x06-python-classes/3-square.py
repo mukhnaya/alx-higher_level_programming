620a82915c9d4b2434e592d5c3b623e09b194757
mukhnaya <mosesmukhanya@gmail.com>
2023-04-04 07:20:04 +0000

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,6 +11,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
         def area(self):
             '''get method to return area of square'''
             return size * size
