ce600045801b84a83d8f416646ee02246a8b62cf
mukhnaya <mosesmukhanya@gmail.com>
2023-04-04 07:53:35 +0000

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,9 +14,10 @@ class Square:
 
     def size(self, value):
         self.size = value
-        if type (size) is not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
         return self.__size * self.__size
