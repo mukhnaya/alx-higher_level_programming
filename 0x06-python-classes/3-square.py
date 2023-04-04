072d99be987050b9b2fedb11909d2f82ee8f162e
mukhnaya <mosesmukhanya@gmail.com>
2023-04-04 07:17:46 +0000

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''declaration of class Square'''
+
+
+class Square:
+    '''body of the class square'''
+    def __init__(self, size=0):
+        '''init method to initialize size of square'''
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        def area(self):
+            '''get method to return area of square'''
+            return size * size
