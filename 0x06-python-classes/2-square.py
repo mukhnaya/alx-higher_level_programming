5ce14563b5fe629227c7bf01c5ec9e2c72a58b1a
mukhnaya <mosesmukhanya@gmail.com>
2023-04-04 06:48:47 +0000

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''creates a square class'''
+
+class Square:
+    '''this body is not empty'''
+
+    __size
+    if (type(size) is not int:
+            raise TypeError("size must be an integer")
+    if size < 0:
+    raise ValueError("size must be >= 0")
