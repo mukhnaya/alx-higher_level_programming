8e0f6384dc523c41f702b1f2221dedf2cd0b1d09
mukhnaya <mosesmukhanya@gmail.com>
2023-04-04 15:42:07 +0000

    task 5

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -59,4 +59,7 @@ class Rectangle:
         return "#" * self.__width
 
     def __repr__(self):
-        return ("Rectangle({}, {})".format(self.__width, self.__height))
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+    
+    def __del__(self):
+        print("Bye rectangle...")
