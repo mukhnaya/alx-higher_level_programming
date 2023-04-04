a6a64e69a3f65120088fcb56c542382f7fd83936
mukhnaya <mosesmukhanya@gmail.com>
2023-04-04 07:06:58 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-'''2-square.py: Defines size as an int and also >= 0'''
+'''class declaration'''
 
 
 class Square:
     '''this body is not empty'''
 
-    def __init__(self, size=0)
-    '''initialization of size'''
-    self.__size = size
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
+    def __init__(self, size=0):
+        '''initialization of size'''
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
