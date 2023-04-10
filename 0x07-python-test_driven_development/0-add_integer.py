6b2aec7c3f09cdf8ef09d019caa987de13c0098b
mukhnaya <mosesmukhanya@gmail.com>
2023-04-10 06:40:05 +0000

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,17 +12,3 @@ def add_integer(a, b=98):
             print("a must be an integer")
         if type(b) is not int:
             print("b must be an integer")
-
-
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
