ea8d9f06874a834c4170f4b1ed252253ba245f5b
mukhnaya <mosesmukhanya@gmail.com>
2023-04-10 09:19:20 +0000

    task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+'''To divide matrix elements with a number and return new matrix'''
 def matrix_divided(matrix, div):
     '''
     divides elements of a matrix
     '''
-    if matrix is not None:
+    if matrix is list:
         '''number of rows of matrix'''
         matrix_rows = len(matrix)
         jos = 0
@@ -11,6 +12,8 @@ def matrix_divided(matrix, div):
             for i in matrix[jos]:
                 if (type(i) is not int) | (type(i) is not float):
                     raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                break
+            jos+=1
         '''len of first row'''
         first_row_len = len(matrix[0])
         mos = 0
@@ -20,6 +23,7 @@ def matrix_divided(matrix, div):
             if first_row_len != rows_len:
                 raise TypeError("Each row of the matrix must have the same size")
                 break
+            mos+=1
         if (type(div) is not int) | (type(div) is not float):
             raise TypeError("div must be a number")
         if div == 0:
@@ -29,4 +33,4 @@ def matrix_divided(matrix, div):
             for y in range(first_row_len):
                 res = matrix[x][y] / div
                 new_matrix.append(res)
-        return new_matrix
+            return new_matrix
