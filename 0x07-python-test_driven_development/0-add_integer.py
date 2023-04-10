9a4ad9e8782d01be864e82dbd5633caec02d6115
mukhnaya <mosesmukhanya@gmail.com>
2023-04-10 06:42:11 +0000

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
     try:
-        if(type(a) is float) | (type(b) is float):
+        if (type(a) is float) | (type(b) is float):
             a = int(a)
             b = int(b)
             return a + b
-        elif(type(a) is int) | (type(b) is int):
+        elif (type(a) is int) | (type(b) is int):
             return a + b
     except TypeError:
         if type(a) is not int:
