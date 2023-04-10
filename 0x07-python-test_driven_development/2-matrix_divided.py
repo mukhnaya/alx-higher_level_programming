db2f7fadd1b06cf291a2e13654050c032c71ba3b
mukhnaya <mosesmukhanya@gmail.com>
2023-04-10 08:48:41 +0000

    task 1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    '''
+    divides elements of a matrix
+    '''
+    if matrix is not None:
+        '''number of rows of matrix'''
+        matrix_rows = len(matrix)
+        jos = 0
+        while jos < matrix_rows:
+            for i in matrix[jos]:
+                if (type(i) is not int) | (type(i) is not float):
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        '''len of first row'''
+        first_row_len = len(matrix[0])
+        mos = 0
+        rows_len = 0
+        while (mos < matrix_rows):
+            rows_len = len(matrix[mos])
+            if first_row_len != rows_len:
+                raise TypeError("Each row of the matrix must have the same size")
+                break
+        if (type(div) is not int) | (type(div) is not float):
+            raise TypeError("div must be a number")
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+        new_matrix = [[]]
+        for x in range(matrix_rows):
+            for y in range(first_row_len):
+                res = matrix[x][y] / div
+                new_matrix.append(res)
+        return new_matrix
+
+matrix = [
+            [1, 2, 3],
+                [4, 5, 6]
+                ]
+print(matrix_divided(matrix, 3))
+print(matrix)
