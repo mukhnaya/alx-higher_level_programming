60319f746e932e64200b73bc355524a0060c1bab
mukhnaya <mosesmukhanya@gmail.com>
2023-04-11 20:00:32 +0000

    task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     '''using isinstance function'''
-    if type(obj) ==  a_class:
+    if type(obj) == a_class:
         return True
     else:
         return False
