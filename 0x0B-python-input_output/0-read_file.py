a9b9260b2505bf07fa9410697e943fb56004406b
mukhnaya <mosesmukhanya@gmail.com>
2023-04-12 09:41:45 +0000

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''function that reads from a file'''
+
+
+def read_file(filename=""):
+    '''open a file'''
+    with open('filename', mode='r', encoding='utf-8') as mos:
+        print(mos.read())
