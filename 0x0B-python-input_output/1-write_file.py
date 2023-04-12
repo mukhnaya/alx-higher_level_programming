ab502f98a90fc1503286e9ddde202fa1e3280827
mukhnaya <mosesmukhanya@gmail.com>
2023-04-12 10:19:50 +0000

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,6 +9,3 @@ def write_file(filename="", text=""):
     with open(filename, 'w', encoding='utf-8') as mos:
         mos.write(text)
         return len(text)
-
-nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
-print(nb_characters)
