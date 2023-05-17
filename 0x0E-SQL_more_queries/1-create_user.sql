dcfe384e160cd804eec04969a41ac06212a541aa
mukhnaya <mosesmukhanya@gmail.com>
2023-05-17 16:26:22 +0000

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- create user and assign privileges
 IF NOT EXISTS CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
