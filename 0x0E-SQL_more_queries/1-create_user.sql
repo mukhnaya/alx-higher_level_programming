6c181979852b7eaf3789684fcecaefef1fd2b66c
mukhnaya <mosesmukhanya@gmail.com>
2023-05-17 23:29:05 +0000

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- create user and assign privileges
-IF NOT EXISTS CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
