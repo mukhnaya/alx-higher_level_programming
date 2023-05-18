0b964a34101c588f2b60373bdaa967951bc2a304
mukhnaya <mosesmukhanya@gmail.com>
2023-05-18 00:12:06 +0000

    task 6

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,5 @@
+-- create db and tables
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+	state_id INT NOT NULL FOREIGN KEY(id) REFERENCES states(id))
