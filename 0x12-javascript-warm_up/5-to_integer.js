e51128e24948ca6eefb652b2ebee70e10fb074b6
mukhnaya <mosesmukhanya@gmail.com>
2023-06-13 02:45:38 +0000

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,6 @@ const firstArg = parseInt(process.argv[2]);
 if (isNaN(firstArg)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', process.argv[2]);
+  const arg1 = Math.trunc(process.argv[2]);
+  console.log('My number: ', arg1);
 }
