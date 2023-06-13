d2d03f74fa1f72c90bef87398c1bd1082c8f07dd
mukhnaya <mosesmukhanya@gmail.com>
2023-06-13 23:48:27 +0000

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const list1 = [];
-  for (let k = list.length - 1; k > 0; k--) {
+  for (let k = list.length - 1; k >= 0; k--) {
     list1.push(list[k]);
   }
   return list1;
