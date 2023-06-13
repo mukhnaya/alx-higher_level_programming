e135581b7ddf8748685244089f87343a406a70c3
mukhnaya <mosesmukhanya@gmail.com>
2023-06-13 22:56:23 +0000

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.wdith = w;
+    if (w >= 1 && h >= 1) {
+      this.width = w;
       this.height = h;
     }
   }
